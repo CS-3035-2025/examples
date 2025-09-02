11205e51feb26c6b04f744526bb998a4043311e1
ColeCampbellUNB <114084266+ColeCampbellUNB@users.noreply.github.com>
2025-09-02 10:39:39 -0300

    Remove SK files

--- simplekit/__tests__/something.ts
+++
@@ -1,9 +0,0 @@
-import { startSimpleKit } from "canvas-mode";
-
-test("basic test", () => {
-  expect(startSimpleKit()).toBe(false);
-});
-
-test("placeholder test", () => {
-  expect(undefined).toBeUndefined();
-});
