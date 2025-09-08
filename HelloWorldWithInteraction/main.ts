91900798f7d5e452969be69a3e5c82ec4af000f0
ColeCampbellUNB <114084266+ColeCampbellUNB@users.noreply.github.com>
2025-09-08 14:02:09 -0300

    Added HelloWorld examples

---
+++ HelloWorldWithInteraction/main.ts
@@ -0,0 +1,80 @@
+/**
+ * This is a simple example of how to create a simple form with a textfield and a button,
+ * and display a message when the button is clicked.
+ */
+import {
+  startSimpleKit,
+  setSKRoot,
+  SKButton,
+  SKContainer,
+  SKLabel,
+  SKTextfield,
+  Layout,
+  Settings,
+} from "../simplekit/src/imperative-mode";
+
+// global debug flag to visualize box model dimensions
+// Settings.debug = true;
+// Settings.debugLayout = true;
+// Settings.layoutWarning = true;
+
+const root = new SKContainer();
+root.padding = 10;
+root.fill = "lightgrey";
+
+// fixed size panel in centre
+const panel = new SKContainer({ width: 400, height: 110 });
+panel.fill = "white";
+panel.border = "black";
+panel.padding = 20;
+panel.layoutMethod = new Layout.WrapRowLayout({ gap: 10 });
+//panel.layoutMethod = new Layout.FillRowLayout({ gap: 10 }); //try a different layout
+
+root.addChild(panel);
+root.layoutMethod = new Layout.CentredLayout();
+// try this too
+// root.layoutMethod = new Layout.FillRowLayout();
+// panel.fillWidth = 1;
+console.log(root.layoutWidth);
+
+panel.margin = 50;
+
+// label
+const nameInputLabel = new SKLabel({
+  text: "Name:",
+  align: "right",
+  width: 80,
+});
+
+const outputLabel = new SKLabel({
+  text: "",
+  align: "centre",
+  width: 300,
+});
+
+// texfield
+const name = new SKTextfield({ width: 150 });
+name.fillWidth = 1;
+
+// button
+const hello = new SKButton({ text: "Hello", width: 80 });
+
+// set an event handler for button "action" event
+hello.addEventListener("action", () => {
+  const msg = name.text == "" ? "Enter a name!" : `Hello ${name.text}`;
+  console.log(msg);
+  outputLabel.text = msg;
+  console.log(root.layoutWidth);
+});
+
+root.addEventListener("resize", (e) => {console.log(root.layoutWidth)});
+
+// add them to panel
+panel.addChild(nameInputLabel);
+panel.addChild(name);
+panel.addChild(hello);
+panel.addChild(outputLabel);
+
+setSKRoot(root);
+
+startSimpleKit();
